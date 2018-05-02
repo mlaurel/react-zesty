a6fea6d4476fe7fc817938a68033d5723d8e2268
Grant Glidewell <grant.glidewell@zesty.io>
2018-05-01 18:54:01 -0700

    added: sample env, working fetch

--- src/components/App.js
+++ src/components/App.js
@@ -4,14 +4,25 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
-      apiData: []
+      apiData: {}
     }
   }
   componentDidMount(){
-    // be sure to configure your env
-    // fetch('')
-    //   .then()
-    //   .catch()
+    const {
+      zestyDomain,
+      zestyHash,
+      zestyID
+    } = this.getEnvValues()
+    fetch(`https://${zestyHash}${zestyDomain}/z/content/${zestyID}.json`)
+      .then(res => {
+        return res.json()
+      })
+      .then(data => {
+        this.setState({apiData: data})
+      })
+      .catch(err => {
+        console.error(err)
+      })
   }
   getEnvValues() {
     if (!process.env.REACT_APP_ZESTY_ID
@@ -31,6 +42,7 @@ class App extends Component {
       <React.Fragment>
       <h1>Zesty.io</h1>
       <p>your data returned from the API-</p>
+      <span>{JSON.stringify(this.state.apiData)}</span>
       </React.Fragment>
     )
   }
